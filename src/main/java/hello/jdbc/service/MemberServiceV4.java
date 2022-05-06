38cc2f22e906368673ed23b5295e09c82ebca4ea
lee <anomalocaris3871@gmail.com>
2022-05-06 17:17:21 +0900

    Add MemberService4

---
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,35 @@
+
+package hello.jdbc.service;
+
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Slf4j
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        if (toMember.getMemberId().equals("memberEX")) {
+            throw new IllegalStateException("transfer exception occurs");
+        }
+
+        memberRepository.update(toId, toMember.getMoney() + money);
+
+    }
+}
